ee6efd53422c0a8b1d8e8d7e5581f2a443391046
manas-z <manasdzambre@gmail.com>
2025-11-23 13:04:51 +0530

    Some critical changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCXVeZQSQos2_wbRHfKFxKI2cL4HnPSvxg",
-  authDomain: "cyclefit-new.firebaseapp.com",
-  projectId: "cyclefit-new",
-  storageBucket: "cyclefit-new.firebasestorage.app",
-  messagingSenderId: "443384351748",
-  appId: "1:443384351748:web:7219c7a1591e92a8ddf951"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
